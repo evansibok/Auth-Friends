51e0631cfd25a7950170858a9266fecc7ce497f5
evansibok <evansibok@gmail.com>
2019-10-15 15:48:45 +0100

    working on adding the token to localStorage

--- friends/src/components/login.jsx
+++ friends/src/components/login.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import axios from 'axios';
 
 const Login = () => {
 
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+
+  const submitHandler = (props) => {
+    axios.post("http://localhost:5000", {
+      username: usernameRef.current.value(),
+      password: usernameRef.current.value()
+    })
+      .then(res => {
+        localStorage.getItem('token', res.data.token);
+
+        props.history.push("/api/friends");
+      })
+  }
   return (
     <div>
-
-      <input type="text" placeholder="Username" />
+      <input type="text" 
+      ref={usernameRef} 
+      placeholder="Username" />
       <br />
 
-      <input type="password" placeholder="Password" />
+      <input type="password" 
+      ref={passwordRef} 
+      placeholder="Password" />
       <br />
-      
+
       <button type="button">Login</button>
     </div>
   );
